c2cc30f3ab2f98b57dcc39153200c753f7695e37
fengbao.wang <fengbao.wang@alo7.com>
2020-09-07 09:43:31 +0800

    add Type example
    add Type example

---
+++ src/com/honestAnt/helloworld/TypeExample.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+/**
+*Go 支持的基本类型：
+bool
+数字类型
+	int8, int16, int32, int64, int
+	uint8, uint16, uint32, uint64, uint
+	float32, float64
+	complex64, complex128
+	byte
+	rune
+string
+*/
+func main() {
+
+}
+
+func booleanTest() {
+	a := true
+	b := false
+	fmt.Println("a: ", a, " b: ", b)
+
+}
